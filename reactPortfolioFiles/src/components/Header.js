58964f89f0523edb6184d3fbec96c05e2f1a9e5a
Summy <114129193+summmyy@users.noreply.github.com>
2022-12-08 20:48:46 -0500

    Add files via upload

--- reactPortfolioFiles/src/components/Header.js
+++ reactPortfolioFiles/src/components/Header.js
@@ -32,91 +32,90 @@ const socials = [
   },
 ];
 
-function useScrollDirection() {
-  const [scrollDirection, setScrollDirection] = useState(null);
-
-  const myRef = useRef();
-
-  useEffect(() => {
-    const lastScrollY = window.pageYOffset;
-
-    const handleScroll = () => {
-      const scrollY = window.pageYOffset
-      const direction = scrollY > lastScrollY ? "down" : "up";
-      if (direction !== scrollDirection && (scrollY - lastScrollY > 10 || scrollY - lastScrollY < -10)) {
-        setScrollDirection(direction);
-      }
-      lastScrollY = scrollY > 0 ? scrollY : 0;
-    };
-    window.addEventListener('scroll', handleScroll)
-    return () => {
-      window.removeEventListener('scroll', handleScroll)
-    
-    }
-  }, [scrollDirection]);
+const Header = () => { 
+  const headerRef = useRef(null); 
   
-  return scrollDirection;
-};
-
-const Header = () => {
-  const handleClick = (anchor) => () => {
-    const id = `${anchor}-section`;
-    const element = document.getElementById(id);
-    if (element) {
-      element.scrollIntoView({
-        behavior: "smooth",
-        block: "start",
-      });
-    }
-  };
-
-  return (
-    <Box
-      
-      position="fixed"
-      top={0}
-      left={0}
-      right={0}
-      translateY={0}
-      transitionProperty="transform"
-      transitionDuration=".3s"
-      transitionTimingFunction="ease-in-out"
-      backgroundColor="#18181b"
-      useScrollDirection={useScrollDirection}
-    >
-      <Box color="white" maxWidth="1280px" margin="0 auto">
-        <HStack
-          px={16}
-          py={4}
-          justifyContent="space-between"
-          alignItems="center"
-        >
-          <nav>
-            <HStack spacing={8}>
-          {socials.map(({icon, url}) => {
-              return (
-                <a
-                  key={url}
-                  className="social-links"
-                  target="_blank"
-                  href={url}
-                  rel="noopener noreferrer"
-                >
-                  <FontAwesomeIcon icon={icon} size="2x" key={url} />
-                </a>
-              );
-            })}
-            </HStack>
-          </nav>
-          <nav>
-            <HStack spacing={8}>
-              <a href="/#contactme" onClick={handleClick("contactme")}>Contact Me</a>
-              <a href="/#projects" onClick={handleClick("projects")}>Projects</a>
-            </HStack>
-          </nav>
-        </HStack>
-      </Box>
-    </Box>
-  );
-};
-export default Header;
+  useEffect(() => { 
+    let prevScrollPos = window.scrollY; 
+  
+    const handleScroll = () => { 
+      const currentScrollPos = window.scrollY; 
+      const headerElement = headerRef.current; 
+      if (!headerElement) { 
+        return; 
+      } 
+      if (prevScrollPos > currentScrollPos) { 
+        headerElement.style.transform = "translateY(0)"; 
+      } else { 
+        headerElement.style.transform = "translateY(-200px)"; 
+      } 
+      prevScrollPos = currentScrollPos; 
+    } 
+    window.addEventListener('scroll', handleScroll) 
+  
+    return () => { 
+      window.removeEventListener('scroll', handleScroll) 
+    } 
+  }, []); 
+  
+  const handleClick = (anchor) => () => { 
+    const id = `${anchor}-section`; 
+    const element = document.getElementById(id); 
+    if (element) { 
+      element.scrollIntoView({ 
+        behavior: "smooth", 
+        block: "start", 
+      }); 
+    } 
+  }; 
+  return ( 
+    <Box 
+      position="fixed" 
+      top={0} 
+      left={0} 
+      right={0} 
+      translateY={0} 
+      transitionProperty="transform" 
+      transitionDuration=".3s" 
+      transitionTimingFunction="ease-in-out" 
+      backgroundColor="#18181b" 
+      ref={headerRef} 
+    > 
+      <Box color="white" maxWidth="1280px" margin="0 auto"> 
+        <HStack 
+          px={16} 
+          py={4} 
+          justifyContent="space-between" 
+          alignItems="center" 
+        > 
+          <nav> 
+            <HStack spacing={8}> 
+              {socials.map(({ icon, url }) => ( 
+                <a 
+                  key={url} 
+                  href={url} 
+                  target="_blank" 
+                  rel="noopener noreferrer" 
+                > 
+                  <FontAwesomeIcon icon={icon} size="2x" key={url} /> 
+                </a> 
+              ))} 
+            </HStack> 
+          </nav> 
+          <nav> 
+            <HStack spacing={8}> 
+              <a href="#projects" onClick={handleClick("projects")}> 
+                Projects 
+              </a> 
+              <a href="#contactme" onClick={handleClick("contactme")}> 
+                Contact Me 
+              </a> 
+            </HStack> 
+          </nav> 
+        </HStack> 
+      </Box> 
+    </Box> 
+  ); 
+ }; 
+ 
+ export default Header; 
